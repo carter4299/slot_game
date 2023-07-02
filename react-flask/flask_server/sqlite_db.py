b8562d14a471142dbe2ee5c669b408398be44893
Carter Susi <133709987+carter4299@users.noreply.github.com>
2023-07-01 22:54:37 -0500

    Update sqlite_db.py

--- react-flask/flask_server/sqlite_db.py
+++ react-flask/flask_server/sqlite_db.py
@@ -1,57 +1,41 @@
 import sqlite3
 import os
-your_path = ''
-your_databse_name = '.db'
+
 class SQL_Util:
     def __init__(self, x):
-        self.db_path = os.path.join({your_path}, your_databse_name)
+        self.db_path = os.path.join(r'C:\Users\-----\OneDrive\Desktop\---------------', '------------')
         self.user_id = x
 
+    def execute_query(self, query, values=None):
+        result = None
+        with sqlite3.connect(self.db_path) as conn:
+            try:
+                c = conn.cursor()
+                if values:
+                    c.execute(query, values)
+                else:
+                    c.execute(query)
+                conn.commit()
+                result = c.fetchone()
+            except sqlite3.Error as e:
+                print(f"An error occurred: {e.args[0]}")
+        return result
+
     def user_validation(self):
-        there = None
-        conn = sqlite3.connect(self.db_path)
-        c = conn.cursor()
-        try:
-            c.execute("SELECT * FROM users WHERE user_id = ?", (self.user_id,)) 
-            data = c.fetchone()
-            if data is not None:
-                there = True
-            else:
-                there = False
-        except sqlite3.Error as e:
-            print(f"An error occurred: {e.args[0]}")
-            there = False
-        finally:
-            conn.close()
+        data = self.execute_query("SELECT * FROM users WHERE user_id = ?", (self.user_id,))
+        return bool(data)
 
-        return there
-    
     def get_user_balance(self):
-        bal = None
-        conn = sqlite3.connect(self.db_path)
-        c = conn.cursor()
-        try:
-            c.execute("SELECT balance FROM users WHERE user_id=?", (self.user_id,))
-            data = c.fetchone()
-            if data is None:
-                bal = None
-            else:
-                bal = data[0]
-        except sqlite3.Error as e:
-            print(f"An error occurred: {e.args[0]}")
-            bal = None
-        finally:
-            conn.close()
+        data = self.execute_query("SELECT balance FROM users WHERE user_id=?", (self.user_id,))
+        return data[0] if data else None
 
-        return bal
-    
     def change_user_balance(self, x):
-        conn = sqlite3.connect(self.db_path)
-        c = conn.cursor()
-        try:
-            c.execute("UPDATE users SET balance = ? WHERE user_id = ?", (x, self.user_id))
-            conn.commit()
-        except sqlite3.Error as e:
-            print(f"An error occurred: {e.args[0]}")
-        finally:
-            conn.close()
+        self.execute_query("UPDATE users SET balance = ? WHERE user_id = ?", (x, self.user_id))
+
+    def change_jackpot_val(self, val):
+        self.execute_query("UPDATE curr_jackpot SET value = ?", (val,))
+
+    def ret_jackpot_val(self):
+        data = self.execute_query("SELECT value FROM curr_jackpot")
+        return data[0] if data else None
+
